de8b8ead813222daddf4487549043eee366efd61
shyeonn <tkdgus2916@gmail.com>
2024-02-23 13:39:00 +0900

    Implement all stage

--- rv32i.h
+++ rv32i.h
@@ -18,17 +18,56 @@
 #define DMEM_DEPTH 1024
 
 // Opcode
-#define I_L-Type 0b0000011
-#define I_A-Type 0b0010011
-#define S-Type 0b0100011
-#define R-Type 0b0110011
-#define U-Type 0b0110111
-#define SB-Type 0b1100011
-#define UJ-Type 0b1101111
+#define I_L_TYPE 0b0000011
+#define I_R_TYPE 0b0010011
+#define S_TYPE 0b0100011
+#define R_TYPE 0b0110011
+#define U_TYPE 0b0110111
+#define SB_TYPE 0b1100011
+#define UJ_TYPE 0b1101111
+#define U_TYPE 0b0110111
 
+// Func3(Loads)
+#define LB 0b000
+
+// Func3(Stores)
+
+// Func3(Arithmetic & Shifts)
+#define F3_SL 0b001
+#define F3_SR 0b101
+#define F3_ADD_SUB 0b000
+#define F3_XOR 0b100
+#define F3_OR 0b110
+#define F3_AND 0b111
+#define F3_SLT 0b010
+#define F3_SLTU 0b011
+
+// Func3(Branches)
+#define F3_BEQ 0b000
+#define F3_BNE 0b001
+#define F3_BLT 0b100
+#define F3_BGE 0b101
+#define F3_BLTU 0b110
+#define F3_BGEU 0b111
+
+// Alu control
+#define C_AND 0b0000
+#define C_OR 0b0001
+#define C_XOR 0b0011
+#define C_SL 0b1000
+#define C_SR 0b1010
+#define C_SRA 0b1011
+#define C_SUB 0b0110
+#define C_ADD 0b0010
 
 // configs
 #define CLK_NUM 45
+
+// Register
+enum REG {
+  READ = 0,
+  WRITE
+};
 
 // structures
 struct imem_input_t {
@@ -54,7 +93,6 @@ struct regfile_input_t {
 	uint8_t rs2;
 	uint8_t rd;
 	uint32_t rd_din;
-	uint8_t reg_write;
 };
 
 struct regfile_output_t {
@@ -72,4 +110,15 @@ struct alu_output_t {
 	uint32_t result;
 	uint8_t zero;
 	uint8_t sign;
+};
+
+struct dmem_input_t {
+  uint32_t addr;
+  uint32_t din;
+  uint8_t mem_write;
+  uint8_t mem_read;
+};
+
+struct dmem_output_t {
+  uint32_t dout;
 };
--- rv32i_single.c
+++ rv32i_single.c
@@ -11,8 +11,10 @@
 #define P_DEBUG printf
 
 struct imem_output_t imem(struct imem_input_t imem_in, uint32_t *imem_data);
-struct control_output_t control_unit(uint32_t opcode);
-struct regfile_output_t regfile(struct regfile_input_t regfile_in, uint32_t *reg_data);
+struct regfile_output_t regfile(struct regfile_input_t regfile_in, uint32_t *reg_data, enum REG regwrite);
+struct alu_output_t alu(struct alu_input_t alu_in);
+uint8_t alu_control_gen(uint8_t opcode, uint8_t func3, uint8_t func7);
+struct dmem_output_t dmem(struct dmem_input_t dmem_in, uint32_t *dmem_data);
 
 int main (int argc, char *argv[]) {
 
@@ -77,14 +79,26 @@ int main (int argc, char *argv[]) {
 	fclose(f_dmem);
 
 	// processor model
+	// IF
 	uint32_t pc_curr, pc_next;	// program counter
 	struct imem_input_t imem_in;
 	struct imem_output_t imem_out;
 
+	// ID
 	struct regfile_input_t regfile_in;
 	struct regfile_output_t regfile_out;
 	uint8_t opcode;
+	uint8_t func3;
+	uint8_t func7;
 	struct control_output_t control_out;
+
+	// EX
+	struct alu_input_t alu_in;
+	struct alu_output_t alu_out;
+
+	// M
+	struct dmem_input_t dmem_in;
+	struct dmem_output_t dmem_out;
 
 	uint32_t cc = 2;	// clock count
 				
@@ -93,39 +107,99 @@ int main (int argc, char *argv[]) {
 
 	while (cc < CLK_NUM) {
 		// instruction fetch
+		pc_curr = pc_next;
 		imem_in.addr = pc_curr;
-		pc_next = pc_curr + 4;
 
 		imem_out = imem(imem_in, imem_data);
-		P_DEBUG("dout : %08X\n", imem_out.dout);
 
 		// instruction decode
 		opcode = imem_out.dout & 0x3F;
-		P_DEBUG("opcode : %x\n", opcode);
 
-		//control_out = control_unit(opcode);
+		if (!(opcode == U_TYPE && opcode == UJ_TYPE))
+			func3 = (imem_out.dout >> 12) & 0x7;
+		if (opcode == R_TYPE)
+			func7 = (imem_out.dout >> 25) & 0x7F;
 
 		regfile_in.rs1 = (imem_out.dout >> 15) & 0x3F;
 		regfile_in.rd = (imem_out.dout >> 7) & 0x3F;
 
-		if (opcode == SB-Type || opcode == R-Type)
-		  regfile_in.rs2 = (imem_out.dout >> 20) & 0x3F;
+		if (opcode == SB_TYPE || opcode == R_TYPE)
+			regfile_in.rs2 = (imem_out.dout >> 20) & 0x3F;
 
-		//Immediate generation
-		if (opcode == U-Type)
-		  alu_in.in2 = (imem_out.dout >> 12) & 0xFFFFF;
-		else if (opcode == I_L-Type || opcode == I_R-Type)
-		  alu_in.in2 = (imem_out.dout >> 20) & 0xFFF;
-		
 		regfile_out = regfile(regfile_in, reg_data, READ);
 
-//		// execution
-//		alu_out = alu(alu_in);
-//		// memory
-//		dmem_out = dmem(dmem_in, dmem_data);
-//		// write-back
-		
-		pc_curr = pc_next;
+		alu_in.in1 = regfile_out.rs1_dout;
+
+		//Immediate generation
+		if (opcode == U_TYPE)
+			alu_in.in2 = (imem_out.dout >> 12) & 0xFFFFF;
+		else if (opcode == I_L_TYPE || opcode == I_R_TYPE)
+			alu_in.in2 = (imem_out.dout >> 20) & 0xFFF;
+		else
+			alu_in.in2 = regfile_out.rs2_dout;
+
+		// execution
+		alu_in.alu_control = alu_control_gen(opcode, func3, func7);
+		alu_out = alu(alu_in);
+
+		// memory
+		if(opcode == S_TYPE || opcode == I_L_TYPE){
+			dmem_in.addr = alu_out.result;
+			dmem_in.din = regfile_out.rs2_dout;
+
+			if(opcode == S_TYPE)
+				dmem_in.mem_write = 1;
+			else
+				dmem_in.mem_write = 0;
+
+			dmem_out = dmem(dmem_in, dmem_data);
+		}
+
+		// write-back
+		if(!(opcode == SB_TYPE || opcode == S_TYPE)){
+			regfile_in.rd_din = regfile_out.rs2_dout;
+			regfile_out = regfile(regfile_in, reg_data, WRITE);
+		}
+
+		// Program counter
+		pc_next = pc_curr + 4;
+
+		if(opcode == SB_TYPE){
+			uint8_t pc_next_sel = 0;
+			uint32_t r1 = regfile_out.rs1_dout;
+			uint32_t r2 = regfile_out.rs2_dout;
+
+			switch(func3){
+				case F3_BEQ:
+					pc_next_sel = r1 == r2;
+					break;
+				case F3_BNE:
+					pc_next_sel = r1 != r2;
+					break;
+				case F3_BLT:
+					pc_next_sel = (int32_t)r1 < (int32_t)r2;
+					break;
+				case F3_BGE:
+					pc_next_sel = (int32_t)r1 >= (int32_t)r2;
+					break;
+				case F3_BLTU:
+					pc_next_sel = r1 < r2;
+					break;
+				case F3_BGEU:
+					pc_next_sel = r1 >= r2;
+					break;
+			}
+			if(pc_next_sel){
+				uint32_t imm = 0;
+
+				imm = imm | ((imem_out.dout >> 31) & 0x1) << 12;
+				imm = imm | ((imem_out.dout >> 25) & 0x3F) << 5;
+				imm = imm | ((imem_out.dout >> 8) & 0xF) << 1;
+				imm = imm | ((imem_out.dout >> 7) & 0x1) << 11;
+				
+				pc_next = pc_curr + imm;
+			}
+		}
 		cc++;
 	}
 
@@ -145,89 +219,100 @@ struct imem_output_t imem(struct imem_input_t imem_in, uint32_t *imem_data) {
 	return imem_out;
 }
 
-struct control_output_t control_unit(uint32_t opcode){
-	struct control_output_t control;
-
-	switch (opcode) {
-		//R-Type
-		case 0b0110011:
-			control.branch = 0;
-			control.mem_read = 0;
-			control.memtoreg = 0;
-			control.aluop = 0b10;
-			control.mem_write = 0;
-			control.alu_src = 1;
-			control.reg_write = 1; 
-			break;
-		//I-Type
-		case 0b0010011:
-		case 0b0000011:
-			control.branch = 0;
-			control.mem_read = 1;
-			control.memtoreg = 1;
-			control.aluop = 0b10;
-			control.mem_write = 0;
-			control.alu_src = 1;
-			control.reg_write = 1; 
-			break;
-		//S-Type
-		case 0b0100011:
-			control.branch = 0;
-			control.mem_read = 0;
-			control.memtoreg = 1;
-			control.aluop = 0b00;
-			control.mem_write = 1;
-			control.alu_src = 1;
-			control.reg_write = 0; 
-			break;
-		//SB-Type
-		case 0b1100011:
-			control.branch = 1;
-			control.mem_read = 0;
-			control.memtoreg = 0;
-			control.aluop = 0b10;
-			control.mem_write = 0;
-			control.alu_src = 0;
-			control.reg_write = 0; 
-			break;
-		//U-Type
-		case 0b0110111:
-			control.branch = 0;
-			control.mem_read = 0;
-			control.memtoreg = 0;
-			control.aluop = 0b11;
-			control.mem_write = 0;
-			control.alu_src = 1;
-			control.reg_write = 1; 
-			break;
-		//UJ-Type
-		case 0b1101111:
-			control.branch = 1;
-			control.mem_read = 1;
-			control.memtoreg = 1;
-			control.aluop = 0b11;
-			control.mem_write = 0;
-			control.alu_src = 1;
-			control.reg_write = 1; 
-			break;
-	}
-
-	return control;
-}
-
 struct regfile_output_t regfile(struct regfile_input_t regfile_in, uint32_t *reg_data, enum REG regwrite){
 
 	struct regfile_output_t regfile_out;
 
 	if(regwrite == READ){
-	  regfile_out.rs1_dout = reg_data[regfile_in.rs1];
-
-	  if(regfile_in.rs2 < REG_WIDTH)
-		  regfile_out.rs2_dout = reg_data[regfile_in.rs2];
+		regfile_out.rs1_dout = reg_data[regfile_in.rs1];
+		if(regfile_in.rs2 < REG_WIDTH)
+			regfile_out.rs2_dout = reg_data[regfile_in.rs2];
 	}
 	else if(regwrite  = WRITE){
 		reg_data[regfile_in.rd] = regfile_in.rd_din;
 	}
 
 	return regfile_out;
+}
+
+struct alu_output_t alu(struct alu_input_t alu_in){
+
+	struct alu_output_t alu_out;
+
+	alu_out.zero = 1;
+	alu_out.sign = 1;
+
+	switch(alu_in.alu_control){
+		case C_AND:
+			alu_out.result = alu_in.in1 & alu_in.in2;
+		case C_OR:
+			alu_out.result = alu_in.in1 | alu_in.in2;
+		case C_XOR:
+			alu_out.result = alu_in.in1 ^ alu_in.in2;
+		case C_SL:
+			alu_out.result = alu_in.in1 << (int32_t)alu_in.in2;
+		case C_SR:
+			alu_out.result = alu_in.in1 >> (int32_t)alu_in.in2;
+		case C_SRA:
+			alu_out.result = (int32_t)alu_in.in1 >> (int32_t)alu_in.in2;
+		case C_SUB:
+			alu_out.result = (int32_t)alu_in.in1 - (int32_t)alu_in.in2;
+		default:
+			alu_out.result = (int32_t)alu_in.in1 + (int32_t)alu_in.in2;
+	}
+
+	if(alu_out.result)
+	  alu_out.zero = 0;
+
+	if(alu_out.result > 0)
+	  alu_out.sign = 0;
+	//does not handle the case of sign = 1 when result is zero
+
+	return alu_out;
+}
+
+uint8_t alu_control_gen(uint8_t opcode, uint8_t func3, uint8_t func7){
+
+	if(opcode == I_L_TYPE && opcode == S_TYPE)
+		return C_ADD;
+	else if(opcode == SB_TYPE)
+		return C_SUB;
+	else {
+		switch(func3){
+		//Arithmetic & Shifts
+			case F3_SL:
+				return C_SL;
+			case F3_SR:
+				if(((func7 >> 6) & 1))
+					return C_SUB;
+				else
+					return C_ADD;
+			case F3_ADD_SUB:
+				if(((func7 >> 6) & 1))
+					return C_SRA;
+			else
+				return C_SR;
+			case F3_XOR:
+				return C_XOR;
+			case F3_OR:
+				return C_OR;
+			case F3_AND:
+				return C_AND;
+			case F3_SLT:
+			case F3_SLTU:
+				return C_SUB;
+		}
+	}
+}
+
+struct dmem_output_t dmem(struct dmem_input_t dmem_in, uint32_t *dmem_data) {
+	
+	struct dmem_output_t dmem_out;
+
+	if(dmem_in.mem_read)
+		dmem_out.dout = dmem_data[dmem_in.addr/4];
+	if(dmem_in.mem_write)
+		dmem_data[dmem_in.addr/4] = dmem_in.din;
+
+	return dmem_out;
 }
